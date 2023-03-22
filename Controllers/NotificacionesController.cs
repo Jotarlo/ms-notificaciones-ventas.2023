eff8286c3ce02cf5236c76ab351a65ab26482bb7
jotarlo <Jeferson Arango Lpez>
2023-03-22 17:11:56 -0500

    Ajuste para envío de correo con plantillas diferentes y asuntos diferentes

--- Controllers/NotificacionesController.cs
+++ Controllers/NotificacionesController.cs
@@ -8,24 +8,66 @@ namespace ms_notificaciones.Controllers;
 [Route("[controller]")]
 public class NotificacionesController : ControllerBase
 {
-    [Route("correo")]
+    [Route("correo-bienvenida")]
     [HttpPost]
-    public async Task<ActionResult> EnviarCorreo(ModeloCorreo datos)
+    public async Task<ActionResult> EnviarCorreoBienvenida(ModeloCorreo datos)
     {
-        //var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
-        var apiKey = "";
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
         var client = new SendGridClient(apiKey);
-        var from = new EmailAddress("jeferson.arango@ucaldas.edu.co", "Jeferson Arango L");
+        
+        SendGridMessage msg = this.CrearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            name = datos.nombreDestino,
+            message = "Bienvenido a la comunidad de la inmobiliaria."
+        });
+        var response = await client.SendEmailAsync(msg);
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("Correo enviado a la dirección " + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+        }
+    }
+
+
+    [Route("correo-recuperacion-clave")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarCorreoRecuperacionClave(ModeloCorreo datos)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+        
+        SendGridMessage msg = this.CrearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            name = datos.nombreDestino,
+            message = "Esta es su nuevla clave... no la comparta."
+        });
+        var response = await client.SendEmailAsync(msg);
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("Correo enviado a la dirección " + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+        }
+    }
+
+    private SendGridMessage CrearMensajeBase(ModeloCorreo datos)
+    {
+        var from = new EmailAddress(Environment.GetEnvironmentVariable("EMAIL_FROM"), Environment.GetEnvironmentVariable("NAME_FROM"));
         var subject = datos.asuntoCorreo;
         var to = new EmailAddress(datos.correoDestino, datos.nombreDestino);
         var plainTextContent = "plain text content";
         var htmlContent = datos.contenidoCorreo;
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-        var response = await client.SendEmailAsync(msg);
-        if(response.StatusCode == System.Net.HttpStatusCode.Accepted){
-            return Ok("Correo enviado a la dirección " + datos.correoDestino);
-        }else{
-            return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
-        }
+        return msg;
     }
+
 }
--- Models/ModeloCorreo.cs
+++ Models/ModeloCorreo.cs
@@ -2,9 +2,9 @@ namespace ms_notificaciones.Models
 {
     public class ModeloCorreo
     {
-        public string correoDestino { set; get; }
-        public string nombreDestino { set; get; }
-        public string asuntoCorreo { set; get; }
-        public string contenidoCorreo { set; get; }
+        public string? correoDestino { set; get; }
+        public string? nombreDestino { set; get; }
+        public string? asuntoCorreo { set; get; }
+        public string? contenidoCorreo { set; get; }
     }
 }
