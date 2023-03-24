f7af1a28ab3eed06159706df8a7903f52337341f
jotarlo <Jeferson Arango Lpez>
2023-03-23 22:27:43 -0500

    Se ajusta para enviar sms con AWS

--- Controllers/NotificacionesController.cs
+++ Controllers/NotificacionesController.cs
@@ -2,6 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using ms_notificaciones.Models;
+using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+
+
 namespace ms_notificaciones.Controllers;
 
 [ApiController]
@@ -14,7 +19,7 @@ public class NotificacionesController : ControllerBase
     {
         var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
         var client = new SendGridClient(apiKey);
-        
+
         SendGridMessage msg = this.CrearMensajeBase(datos);
         msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
         msg.SetTemplateData(new
@@ -40,7 +45,7 @@ public class NotificacionesController : ControllerBase
     {
         var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
         var client = new SendGridClient(apiKey);
-        
+
         SendGridMessage msg = this.CrearMensajeBase(datos);
         msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
         msg.SetTemplateData(new
@@ -59,15 +64,79 @@ public class NotificacionesController : ControllerBase
         }
     }
 
+
+    [Route("enviar-correo-2fa")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarCorreo2fa(ModeloCorreo datos)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+
+        SendGridMessage msg = this.CrearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("TwoFA_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            nombre = datos.nombreDestino,
+            mensaje = datos.contenidoCorreo,
+            asunto = datos.asuntoCorreo
+        });
+        var response = await client.SendEmailAsync(msg);
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("Correo enviado a la dirección " + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+        }
+    }
+
+
     private SendGridMessage CrearMensajeBase(ModeloCorreo datos)
     {
         var from = new EmailAddress(Environment.GetEnvironmentVariable("EMAIL_FROM"), Environment.GetEnvironmentVariable("NAME_FROM"));
         var subject = datos.asuntoCorreo;
         var to = new EmailAddress(datos.correoDestino, datos.nombreDestino);
-        var plainTextContent = "plain text content";
+        var plainTextContent = datos.contenidoCorreo;
         var htmlContent = datos.contenidoCorreo;
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
         return msg;
     }
+
+    // Envío de SMS
+
+    [Route("enviar-sms")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarSMSNuevaClave(ModeloSms datos)
+    {
+        var accessKey = Environment.GetEnvironmentVariable("ACCESS_KEY_AWS");
+        var secretKey = Environment.GetEnvironmentVariable("SECRET_KEY_AWS");
+        var client = new AmazonSimpleNotificationServiceClient(accessKey, secretKey, RegionEndpoint.USEast1);
+        var messageAttributes = new Dictionary<string, MessageAttributeValue>();
+        var smsType = new MessageAttributeValue
+        {
+            DataType = "String",
+            StringValue = "Transactional"
+        };
+
+        messageAttributes.Add("AWS.SNS.SMS.SMSType", smsType);
+
+        PublishRequest request = new PublishRequest
+        {
+            Message = datos.contenidoMensaje,
+            PhoneNumber = datos.numeroDestino,
+            MessageAttributes = messageAttributes
+        };
+        try
+        {
+            await client.PublishAsync(request);
+            return Ok("Mensaje enviado");
+        }
+        catch
+        {
+            return BadRequest("Error enviando el sms");
+        }
+    }
+
 
 }
