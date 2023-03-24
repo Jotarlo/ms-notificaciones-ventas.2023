2b94bda1e71fbdce6e6f60345a8115f3210c6e4c
jotarlo <Jeferson Arango Lpez>
2023-03-23 22:29:33 -0500

    Modelo de sms

---
+++ Models/ModeloSms.cs
@@ -0,0 +1,8 @@
+namespace ms_notificaciones.Models
+{
+    public class ModeloSms
+    {
+        public string? numeroDestino { set; get; }
+        public string? contenidoMensaje { set; get; }
+    }
+}
